d9b3ab86f492c93f9f304dc1a6b14a94acb0f1a3
Adarsh Gupta <109175185+Adarsh-Gupta-3000@users.noreply.github.com>
2024-03-10 02:01:58 +0530

    Delete public/components directory

--- public/components/Loading.js
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-  return (
-    <div className="w-screen h-[calc(100vh-80px)] flex justify-center items-center">
-      <div className="spinner"></div>
-    </div>
-  );
-};
-
-export default Loading;
